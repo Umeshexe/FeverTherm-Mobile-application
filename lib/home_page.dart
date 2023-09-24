2d203f980c39939fe17b3e65430a85127ccdc737
Umesh Chandra <kadaliumeshchandra@gmail.com>
2023-09-24 19:00:26 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Umesh";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Temperature App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,17 @@
+import 'package:fever_therm/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+
+  
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to my app"),
-            ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }

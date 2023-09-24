3973ea84785eadb9f74987f309201a0f684d2342
Umesh Chandra <kadaliumeshchandra@gmail.com>
2023-09-24 20:16:13 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:fever_therm/home_page.dart';
+import 'package:fever_therm/pages/home_page.dart';
+import 'package:fever_therm/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,10 +9,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-  
     return MaterialApp(
-      home: HomePage(),
-    );
+        themeMode: ThemeMode.light,
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+        ),
+        initialRoute: "/home",
+        routes: {
+          "/": (context) => LoginPage(),
+          "/home": (context) => HomePage(),
+          "/login": ((context) => LoginPage()),
+        });
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Temperature App"),
+        title: Text("Thermistor App"),
       ),
       body: Center(
         child: Container(
